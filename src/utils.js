516c29ac4f39ebd45a5bc4beec8f02fc5546f024
alice-shamaeva <alice.shamaeva@gmail.com>
2024-06-02 17:10:13 +0400

    one of last fixes 3

--- src/utils.js
+++ src/utils.js
@@ -34,7 +34,6 @@ const getDifferentObject = (object1, object2) => (
         newValue,
       };
     }
-    
     const oldValue = object1[key];
     const newValue = object2[key];
     if (_.isPlainObject(oldValue) && _.isPlainObject(newValue)) {
