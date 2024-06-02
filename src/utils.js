e49d927e550eb39580415c1872778a55951d118f
alice-shamaeva <alice.shamaeva@gmail.com>
2024-06-02 17:07:14 +0400

    one of last fixes

--- src/utils.js
+++ src/utils.js
@@ -15,22 +15,28 @@ const getExtension = (fileName) => {
 
 const getDifferentObject = (object1, object2) => (
   _.sortBy(_.union(_.keys(object1), _.keys(object2))).map((key) => {
-    const oldValue = object1[key];
-    const newValue = object2[key];
-    if (!_.has(object2, key)) {
+    if (!_.has(object2, key) && _.has(object1, key)) {
+      const oldValue = object1[key];
+
       return {
         action: 'deleted',
         key,
         oldValue,
       };
     }
-    if (!_.has(object1, key)) {
+
+    if (!_.has(object1, key) && _.has(object2, key)) {
+      const newValue = object2[key];
+
       return {
         action: 'added',
         key,
         newValue,
       };
     }
+      
+    const oldValue = object1[key];
+    const newValue = object2[key];
     if (_.isPlainObject(oldValue) && _.isPlainObject(newValue)) {
       return {
         action: 'nested',
